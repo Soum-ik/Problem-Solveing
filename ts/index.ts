13781785b48b40c6c867cc4c2ead99934adcab38
Stack Soumik. <110479389+Soum-ik@users.noreply.github.com>
2025-02-28 16:19:20 +0600

    Add initial implementation of createCounter and expect functions; include launch configuration for debugging

--- ts/index.ts
+++ ts/index.ts
@@ -1,18 +1,79 @@
-function createCounter(n: number): () => number {
-  let counter = n;
-  return function () {
-    return counter++;
-  }
-}
+// £££££££££££££££££££££££££££££££££££££££££
+// function createCounter(n: number): () => number {
+//   let counter = n;
+//   return function () {
+//     return counter++;
+//   }
+// }
 
-const counter = createCounter(10)
-console.log(counter());
-console.log(counter());
-console.log(counter());
+// const counter = createCounter(10)
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
 
-/** 
-* const counter = createCounter(10)
-* counter() // 10
-* counter() // 11
-* counter() // 12
+
+// £££££££££££££££££££££££££££££££££££££££
+// type ToBeOrNotToBe = {
+//   toBe: (val: any) => boolean;
+//   notToBe: (val: any) => boolean;
+// };
+
+// function expect(val: any): ToBeOrNotToBe {
+//   let prams = val;
+//   return {
+//     toBe(val) {
+//       if (val === prams) {
+//         return true
+//       } else {
+//         return false
+//       }
+//     },
+//     notToBe(val) {
+//       if (val !== prams) {
+//         return true
+//       } else {
+//         throw 'Equal'
+//       }
+//     },
+//   }
+// };
+
+/**
+* expect(5).toBe(5); // true
+* expect(5).notToBe(5); // throws "Equal"
 */
+
+// ££££££££££££££££££££££££££££££££££££££
+
+// type Counter = {
+//   increment: () => number,
+//   decrement: () => number,
+//   reset: () => number,
+// }
+
+// function createCounter(init: number): Counter {
+//   let count = init;
+
+//   function increment() {
+//     count++
+//     return count
+//   }
+//   function decrement() {
+//     count--
+//     return count
+//   }
+//   function reset() {
+//     count = init
+//     return count
+//   }
+//   return { increment, decrement, reset }
+// };
+
+// const counter = createCounter(5)
+// console.log(counter.increment());
+// console.log(counter.reset());
+// console.log(counter.decrement());
+
+// ££££££££££££££££££££££££££££££££££££££
+
+

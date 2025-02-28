c1fd55b5021d5927854e764e39894a6a55aef96e
Stack Soumik. <110479389+Soum-ik@users.noreply.github.com>
2025-02-28 21:36:23 +0600

    Add implementation of reduce function; include commented-out map and filter functions for reference

--- ts/index.ts
+++ ts/index.ts
@@ -77,3 +77,46 @@
 // ££££££££££££££££££££££££££££££££££££££
 
 
+// function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+//   let newArr: number[] = [];
+//   arr.forEach((element, index) => {
+//     // Apply the transformation function to each element and its index
+//     const transformedElement = fn(element, index);
+//     // Store the result in the new array
+//     newArr.push(transformedElement);
+//   });
+
+//   return newArr;
+// }
+
+// // Example usage
+// console.log(map([1, 2, 3], function plusone(n) { return n + 1; })); // Output: [2, 3, 4]
+// console.log(map([1, 2, 3], function plusI(n, i) { return n + i; })); // Output: [1, 3, 5]
+// console.log(map([10, 20, 30], function constant() { return 42; })); // Output: [42, 42, 42]
+
+
+///////////////////////////////////////////////
+
+// type Fn = (n: number, i: number) => any
+
+// function filter(arr: number[], fn: Fn): number[] {
+//   const result = arr.filter((n, i) => {
+//     return fn(n, i)
+//   })
+//   return result
+// };
+
+// Input: arr = [0,10,20,30], fn = function greaterThan10(n) { return n > 10; }
+
+
+///////////////////////////////////////////////
+
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+  let res = init;
+  for (let i = 0; i < nums.length; i++) {
+    res = fn(res, nums[i]);
+  }
+  return res;
+};
